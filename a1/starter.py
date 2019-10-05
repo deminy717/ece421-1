dc724c38591d745347aa0d54bc37ffe762210527
eatawing <yli191096@gmail.com>
2019-10-05 01:25:19 -0400

    Update starter.py

--- a1/starter.py
+++ a1/starter.py
@@ -1,3 +1,4 @@
+#%%
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
@@ -22,27 +23,78 @@ def loadData():
     return trainData, validData, testData, trainTarget, validTarget, testTarget
 
 def MSE(W, b, x, y, reg):
-    # Your implementation here
+    N = np.shape(y)[0]
+    e = np.dot(x, W) + b - y
+    return np.sum(e ** 2) / N + (reg / 2) * np.sum(W ** 2)
 
 def gradMSE(W, b, x, y, reg):
-    # Your implementation here
+    N = np.shape(y)[0]
+    e = np.dot(x, W) + b - y
+    
+    grad_b = (2 / N) * np.sum(e)
+    grad_W = (2 / N) * np.dot(e, x) + reg * W
 
-def grad_descent(W, b, x, y, alpha, epochs, reg, error_tol):
-    # Your implementation here
+    return grad_W, grad_b
 
-def crossEntropyLoss(W, b, x, y, reg):
-    # Your implementation here
+def grad_descent(W, b, x, y, alpha, epochs, reg, error_tol, validData, validTarget, testData, testTarget):
+    trainLossHistory = [MSE(W, b, x, y, reg)]
+    validLossHistory = [MSE(W, b, validData, validTarget, reg)]
+    testLossHistory = [MSE(W, b, testData, testTarget, reg)]
 
-def gradCE(W, b, x, y, reg):
-    # Your implementation here
+    for i in range(epochs):
+        grad_W, grad_b = gradMSE(W, b, x, y, reg)
 
-def buildGraph(loss="MSE"):
-	#Initialize weight and bias tensors
-	tf.set_random_seed(421)
+        dist_W, W, b = getNewData(W, alpha, grad_W, b, grad_b)
+        
+        # print(i)
+        trainLossHistory.append(MSE(W, b, x, y, reg))
+        validLossHistory.append(MSE(W, b, validData, validTarget, reg))
+        testLossHistory.append(MSE(W, b, testData, testTarget, reg))
+        
+        if dist_W < error_tol:
+            break
 
-	if loss == "MSE":
-	# Your implementation
-	
-	elif loss == "CE":
-	#Your implementation here
+    return W, b, trainLossHistory, validLossHistory, testLossHistory
 
+def getNewData(W, alpha, grad_W, b, grad_b):
+    new_W = W - alpha * grad_W
+    dist_W = np.linalg.norm(new_W - W)
+
+    return dist_W, new_W, b - alpha * grad_b
+
+# def crossEntropyLoss(W, b, x, y, reg):
+#     # Your implementation here
+
+# def gradCE(W, b, x, y, reg):
+#     # Your implementation here
+
+def buildGraph(loss, alpha):
+
+    tf.set_random_seed(421)
+
+    if loss == "MSE":
+        W, b, trainLossHistory, validLossHistory, testLossHistory = grad_descent(W, b, x, y, alpha, 5000, 0, 1e-7, validData, validTarget, testData, testTarget)
+        return W, b, trainLossHistory, validLossHistory, testLossHistory
+        
+if __name__ == "__main__":
+    trainData, validData, testData, trainTarget, validTarget, testTarget = loadData()
+    x = trainData.reshape(-1, trainData.shape[1] * trainData.shape[2])
+    y = trainTarget.flatten()
+    W = 0
+    b = 0
+
+    validData = validData.reshape(-1, validData.shape[1] * validData.shape[2])
+    validTarget = validTarget.flatten()
+    testData = testData.reshape(-1, testData.shape[1] * testData.shape[2])
+    testTarget = testTarget.flatten()
+
+    W, b, trainLossHistory, validLossHistory, testLossHistory = grad_descent(W, b, x, y, 0.001, 5000, 0, 1e-7, validData, validTarget, testData, testTarget)
+
+    print(trainLossHistory)
+    print(validLossHistory)
+    print(testLossHistory)
+    n = range(len(trainLossHistory))
+    plt.plot(n, trainLossHistory)
+    plt.plot(n, validLossHistory)
+    plt.plot(n, testLossHistory)
+    plt.show()
